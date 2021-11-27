bbb7e911d132e976874023b929b7b04421cd2961
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-28 00:50:22 +0100

    comments service initial implementation

--- comments/src/index.ts
+++ comments/src/index.ts
@@ -6,8 +6,24 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/posts/:id/comments', (req, res) => {});
+const commentsByPostId: any = {};
 
-app.post('/posts/:id/comments', (req, res) => {});
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const id = randomID();
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  const createdComment = { id, content };
+
+  comments.push(createdComment);
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(createdComment);
+});
 
 app.listen(4001, () => console.log(`Listening on 4001`));
