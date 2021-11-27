bbfdf086987e2f73ac8b97d70a10af7d15365ffa
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-28 00:23:54 +0100

    posts request handlers

--- posts/dist/index.js
+++ posts/dist/index.js
@@ -4,18 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var fs_1 = __importDefault(require("fs"));
 var uuid_1 = require("uuid");
 var bodyParser = require('body-parser')();
 var app = (0, express_1.default)();
 app.use(bodyParser);
 var posts = {};
 app.get('/posts', function (req, res) {
-    res.send(posts);
+    var posts = fs_1.default.readFileSync('posts.json', { encoding: 'utf8' });
+    res.send(JSON.parse(posts));
 });
 app.post('/posts', function (req, res) {
     var id = (0, uuid_1.v4)();
-    var title = req.body;
+    var title = req.body.title;
     posts[id] = { id: id, title: title };
+    fs_1.default.writeFileSync('posts.json', JSON.stringify(posts));
     res.status(201).send(posts[id]);
 });
 app.listen(4000, function () { return console.log("Listening on 4000"); });
--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import { v4 as randomID } from 'uuid';
 const bodyParser = require('body-parser')();
 
@@ -9,15 +10,17 @@ app.use(bodyParser);
 const posts: any = {};
 
 app.get('/posts', (req, res) => {
-  res.send(posts);
+  const posts = fs.readFileSync('posts.json', { encoding: 'utf8' });
+  res.send(JSON.parse(posts));
 });
 
 app.post('/posts', (req, res) => {
   const id = randomID();
-  const title = req.body;
+  const { title } = req.body;
 
   posts[id] = { id, title };
 
+  fs.writeFileSync('posts.json', JSON.stringify(posts));
   res.status(201).send(posts[id]);
 });
 
