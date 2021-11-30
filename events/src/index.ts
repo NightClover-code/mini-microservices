df9837ec971fcfa18d6aee179285d4ed69612e0f
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-30 16:43:02 +0100

    basic code on event bus

--- events/src/index.ts
+++ events/src/index.ts
@@ -1,1 +1,19 @@
-console.log('hey');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { clientAPI, commentsAPI, postsAPI } from './utils';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  commentsAPI.post('/events', event);
+  postsAPI.post('/events', event);
+  clientAPI.post('/events', event);
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => console.log(`Listening on 4005`));
