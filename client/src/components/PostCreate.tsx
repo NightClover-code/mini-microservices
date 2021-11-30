fd66d98397a965c12c4449026d7e6cc83551a230
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-30 01:16:00 +0100

    adding post creation from client side

--- client/src/components/PostCreate.tsx
+++ client/src/components/PostCreate.tsx
@@ -1,10 +1,30 @@
+import { FormEvent, useState } from 'react';
+import { postsAPI } from '../utils';
+
 const PostCreate = () => {
+  const [title, setTitle] = useState('');
+
+  const onSumbitHandler = async (e: FormEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    await postsAPI.post('/posts', {
+      title,
+    });
+
+    setTitle('');
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSumbitHandler}>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary mt-2" type="submit">
           Submit
