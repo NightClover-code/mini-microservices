6d41033e005393b310ef0fe6bbbe8779fd2c58a8
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-12-05 20:26:00 +0100

    adding a moderation service

--- moderation/src/index.ts
+++ moderation/src/index.ts
@@ -1,1 +1,11 @@
-console.log('lmao');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { eventsAPI } from './utils';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {});
+
+app.listen(4003, () => console.log(`Listening on 4003`));
